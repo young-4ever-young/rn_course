fade76023a389ea6d0502bb9377c96ba48996b00
Sergey <sergey@iMac-Sergey.local>
2019-09-05 21:09:07 +0300

    finish basics

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
-import React, {Component} from 'react';
-import {StyleSheet, View} from 'react-native';
+import React, { Component } from "react";
+import { StyleSheet, View } from "react-native";
 
-import PlaceInput from './src/components/PlaceInput/PlaceInput';
-import PlaceList from './src/components/PlaceList/PlaceList';
-import PlaceImage from './src/assets/beautiful-place.jpg';
+import PlaceInput from "./src/components/PlaceInput/PlaceInput";
+import PlaceList from "./src/components/PlaceList/PlaceList";
+import PlaceDetail from "./src/components/PlaceDetail/PlaceDetail";
 
 export default class App extends Component {
   state = {
     places: [],
+    selectedPlace: null
   };
 
   placeAddedHandler = placeName => {
@@ -16,18 +17,38 @@ export default class App extends Component {
         places: prevState.places.concat({
           key: Math.random(),
           name: placeName,
-          image: PlaceImage,
-        }),
+          image: {
+            uri:
+              "https://c1.staticflickr.com/5/4096/4744241983_34023bf303_b.jpg"
+          }
+        })
       };
     });
   };
 
-  placeDeletedHandler = key => {
+  placeDeletedHandler = () => {
     this.setState(prevState => {
       return {
         places: prevState.places.filter(place => {
-          return place.key !== key;
+          return place.key !== prevState.selectedPlace.key;
         }),
+        selectedPlace: null
+      };
+    });
+  };
+
+  modalClosedHandler = () => {
+    this.setState({
+      selectedPlace: null
+    });
+  };
+
+  placeSelectedHandler = key => {
+    this.setState(prevState => {
+      return {
+        selectedPlace: prevState.places.find(place => {
+          return place.key === key;
+        })
       };
     });
   };
@@ -35,10 +56,15 @@ export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
+        <PlaceDetail
+          selectedPlace={this.state.selectedPlace}
+          onItemDeleted={this.placeDeletedHandler}
+          onModalClosed={this.modalClosedHandler}
+        />
         <PlaceInput onPlaceAdded={this.placeAddedHandler} />
         <PlaceList
           places={this.state.places}
-          onItemDeleted={this.placeDeletedHandler}
+          onItemSelected={this.placeSelectedHandler}
         />
       </View>
     );
@@ -49,9 +75,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 26,
-    paddingTop: 40,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'flex-start',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "flex-start"
+  }
 });
--- src/components/ListItem/ListItem.js
+++ src/components/ListItem/ListItem.js
@@ -1,14 +1,10 @@
-import React from 'react';
-import {View, Text, StyleSheet, TouchableOpacity, Image} from 'react-native';
+import React from "react";
+import { View, Text, StyleSheet, TouchableOpacity, Image } from "react-native";
 
 const listItem = props => (
   <TouchableOpacity onPress={props.onItemPressed}>
     <View style={styles.listItem}>
-      <Image
-        resizeMode="contain"
-        source={props.placeImage}
-        style={styles.imageItem}
-      />
+      <Image resizeMode="cover" source={props.placeImage} style={styles.placeImage} />
       <Text>{props.placeName}</Text>
     </View>
   </TouchableOpacity>
@@ -16,18 +12,18 @@ const listItem = props => (
 
 const styles = StyleSheet.create({
   listItem: {
-    width: '100%',
+    width: "100%",
     marginBottom: 5,
     padding: 10,
-    backgroundColor: '#eee',
-    flexDirection: 'row',
-    alignItems: 'center',
+    backgroundColor: "#eee",
+    flexDirection: "row",
+    alignItems: "center"
   },
-  imageItem: {
-    width: 30,
-    height: 30,
-    marginRight: 5,
-  },
+  placeImage: {
+      marginRight: 8,
+      height: 30,
+      width: 30
+  }
 });
 
 export default listItem;
